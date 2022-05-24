6e365c5a4bec88e8c9cd9a245a02b6d695e831da
Humza Hasan <humzahasan2101@gmail.com>
2022-05-24 12:49:10 +0530

    feat: result page added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { Landing, Leaderboard, Questions } from "./pages";
+import { Landing, Leaderboard, Questions, Result } from "./pages";
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/questions" element={<Questions />} />{" "}
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/result" element={<Result />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
       <ToastContainer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Landing from "./Landing/Landing";
 import Leaderboard from "./Leaderboard/Leaderboard";
 import Questions from "./Questions/Questions";
+import Result from "./Result/Result";
 import Rules from "./Rules/Rules";
 
-export { Landing, Rules, Questions, Leaderboard };
+export { Landing, Rules, Questions, Result, Leaderboard };
