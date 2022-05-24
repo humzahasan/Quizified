b2639d943c8d8273307a7ebc669068fec04f20eb
Humza Hasan <humzahasan2101@gmail.com>
2022-05-24 14:47:04 +0530

    feat: add and fetching leaderboard db data

--- src/components/QuizCard/QuizCard.jsx
+++ src/components/QuizCard/QuizCard.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useQuiz } from "../../context/quiz-context";
+import { toast } from "react-toastify";
 import "./QuizCard.css";
+
+import { database } from "../../config/firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 const QuizCard = ({ info, questionNum, getNextQuestion, totalQuestion }) => {
   const { question, correctAnswer, incorrectAnswers } = info;
@@ -8,9 +14,39 @@ const QuizCard = ({ info, questionNum, getNextQuestion, totalQuestion }) => {
     () => Math.random() - 0.5
   );
 
-  const updateResult = (option) => {};
+  const navigate = useNavigate();
+  const { setScore, username, category } = useQuiz();
 
-  const submitResult = () => {};
+  const [result, setResult] = useState(0);
+
+  const databaseRef = collection(database, "leaderboard");
+
+  const updateResult = (option) => {
+    if (option === correctAnswer) {
+      toast.success("+10 brownie points for you!");
+      setResult((prev) => prev + 10);
+    } else {
+      toast.error("Ah, oh! -2 on those brownie points");
+      setResult((prev) => prev - 2);
+    }
+  };
+
+  const submitResult = () => {
+    setScore(result);
+    console.log("here submit");
+    addDoc(databaseRef, {
+      username,
+      category,
+      result,
+      timestamp: new Date(),
+    })
+      .then(() => {
+        navigate("/result");
+      })
+      .catch((error) => {
+        toast.error(error);
+      });
+  };
 
   return (
     <div className="card">
--- src/pages/Questions/Questions.jsx
+++ src/pages/Questions/Questions.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, QuizCard } from "../../components";
+import { useQuestion } from "../../context/question-context";
 import "./Questions.css";
 const Questions = () => {
+  const { state } = useQuestion();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const { questions } = state;
+
+  const getNextQuestion = () => {
+    setCurrentQuestion((prev) => prev + 1);
+  };
+
   return (
     <div>
       <Header />
       <div className="questions-container">
-        <QuizCard />
+        <QuizCard
+          info={questions[currentQuestion]}
+          questionNum={currentQuestion}
+          totalQuestion={questions.length}
+          getNextQuestion={getNextQuestion}
+        />
       </div>
     </div>
   );
